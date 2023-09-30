5117df724fdc5c48735d334da650743d6772d0d5
Christopher10TITI <146572784+Christopher10TITI@users.noreply.github.com>
2023-09-30 11:06:08 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,18 @@
-created the main python file
+import streamlit
+import pandas
+
+
+
+streamlit.title('My parents new healty diner')
+
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Advocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.text('Fruits Smoothie 🍓🍌🍍')
+streamlit.text('Green Smoothie 🥦🍏🥒')
+streamlit.text('Protein Smoothie 💪🥤')
+streamlit.text('Blueberry Smoothie 🫐🍇🍓')
